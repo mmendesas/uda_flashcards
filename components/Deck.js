332282fb4f47c48564841c53003cd709e0d978b5
Marcio Mendes <mmendes.as@gmail.com>
2019-05-07 15:42:45 -0300

    chore: lint identation

--- components/Deck.js
+++ components/Deck.js
@@ -5,61 +5,61 @@ import { black, white, gray } from '../utils/colors'
 import { connect } from 'react-redux'
 
 class Deck extends Component {
-    render() {
-        const { deck } = this.props
-        const { title, questions } = deck
+  render() {
+    const { deck } = this.props
+    const { title, questions } = deck
 
-        return (
-            <View style={styles.container}>
-                <Text style={styles.title}>{title}</Text>
-                <Text style={styles.number}>{questions.length} cards</Text>
-                <TouchableOpacity
-                    style={[styles.button, { backgroundColor: white }]}
-                >
-                    <Text>Add Card</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    style={[styles.button, { backgroundColor: black }]}
-                >
-                    <Text style={{ color: white }}>Start Quiz</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.number}>{questions.length} cards</Text>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: white }]}
+        >
+          <Text>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.button, { backgroundColor: black }]}
+        >
+          <Text style={{ color: white }}>Start Quiz</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 35,
-        alignSelf: 'center'
-    },
-    number: {
-        fontSize: 18,
-        alignSelf: 'center',
-        marginBottom: 20,
-        color: gray
-    },
-    button: {
-        alignItems: 'center',
-        padding: 15,
-        marginTop: 15,
-        borderRadius: 15,
-        borderWidth: 1,
-        width: '50%'
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 35,
+    alignSelf: 'center'
+  },
+  number: {
+    fontSize: 18,
+    alignSelf: 'center',
+    marginBottom: 20,
+    color: gray
+  },
+  button: {
+    alignItems: 'center',
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+    borderWidth: 1,
+    width: '50%'
+  },
 })
 
 const mapStateToProps = ({ decks }) => {
-    const deck = decks['React'] //change this later
+  const deck = decks['React'] //change this later
 
-    return {
-        deck
-    }
+  return {
+    deck
+  }
 }
 
 export default connect(mapStateToProps)(Deck)
--- components/NewCard.js
+++ components/NewCard.js
@@ -4,68 +4,68 @@ import { black, white } from '../utils/colors'
 
 class NewCard extends Component {
 
-    state = {
-        question: '',
-        answer: ''
-    }
+  state = {
+    question: '',
+    answer: ''
+  }
 
-    submit = () => {
-        console.log('submit', this.state)
-        this.setState(() => ({ title: '' }))
-    }
+  submit = () => {
+    console.log('submit', this.state)
+    this.setState(() => ({ title: '' }))
+  }
 
-    isDisabled = () => {
-        return this.state.question === '' || this.state.answer === ''
-    }
+  isDisabled = () => {
+    return this.state.question === '' || this.state.answer === ''
+  }
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <TextInput
-                    style={styles.input}
-                    placeholder='Type question here'
-                    value={this.state.question}
-                    onChangeText={(question) => this.setState(() => ({ question }))}
-                />
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          placeholder='Type question here'
+          value={this.state.question}
+          onChangeText={(question) => this.setState(() => ({ question }))}
+        />
 
-                <TextInput
-                    style={styles.input}
-                    placeholder='Type answer here'
-                    value={this.state.answer}
-                    onChangeText={(answer) => this.setState(() => ({ answer }))}
-                />
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={this.submit}
-                    disabled={this.isDisabled()}
-                >
-                    <Text style={{ color: white }}>Submit</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+        <TextInput
+          style={styles.input}
+          placeholder='Type answer here'
+          value={this.state.answer}
+          onChangeText={(answer) => this.setState(() => ({ answer }))}
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.submit}
+          disabled={this.isDisabled()}
+        >
+          <Text style={{ color: white }}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    input: {
-        borderWidth: 1,
-        width: 250,
-        paddingLeft: 10,
-        margin: 20
-    },
-    button: {
-        alignItems: 'center',
-        padding: 15,
-        marginTop: 15,
-        borderRadius: 15,
-        width: 150,
-        backgroundColor: black
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    width: 250,
+    paddingLeft: 10,
+    margin: 20
+  },
+  button: {
+    alignItems: 'center',
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
+    width: 150,
+    backgroundColor: black
+  },
 })
 
 export default NewCard
